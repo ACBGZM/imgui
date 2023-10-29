08f1da3d5d3ce943c81f6370c80c1f7679c23e98
ACBGZM <ACBGZM@126.com>
2023-10-29 13:33:12 +0800

    update

--- premake5.lua
+++ premake5.lua
@@ -1,133 +1,29 @@
-workspace "MiuEngine"
-    architecture "x64"
-    startproject "SandBox"
-    configurations{ "Debug", "Release", "Distribute" }
-
-output_folder = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
--- Include directories relative to root (solution) path
-IncludeDir = {}
-IncludeDir["GLFW"] = "MiuEngine/3rdparty/GLFW/include"
-IncludeDir["Glad"] = "MiuEngine/3rdparty/Glad/include"
-IncludeDir["imgui"] = "MiuEngine/3rdparty/imgui"
-
-group "Dependencies"
-    include "MiuEngine/3rdparty/GLFW"
-    include "MiuEngine/3rdparty/Glad"
-    include "MiuEngine/3rdparty/imgui"
-group ""
-
-project "MiuEngine"
-    location "MiuEngine"
-    kind "SharedLib"
+project "imgui"
+    kind "StaticLib"
     language "C++"
-    staticruntime "off"
-
-    targetdir ("_build/out-bin/" .. output_folder .. "/%{prj.name}")
+    
+	targetdir ("_build/out-bin/" .. output_folder .. "/%{prj.name}")
     objdir ("_build/out-obj/" .. output_folder .. "/%{prj.name}")
 
-    pchheader "pch.h"
-    pchsource "%{prj.name}/src/pch.cpp"
-
-    forceincludes { "pch.h" }
-
-    files{
-        "%{prj.name}/src/**.h", 
-        "%{prj.name}/src/**.cpp"
+	files
+	{
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
     }
-
-    includedirs{
-        "%{prj.name}/src",
-        "%{prj.name}/3rdparty/spdlog/include",
-        "%{IncludeDir.GLFW}",
-        "%{IncludeDir.Glad}",
-        "%{IncludeDir.imgui}",
-    }
-
-    links{
-        "GLFW",
-        "Glad",
-        "imgui",
-        "opengl32.lib",
-        "Dwmapi.lib",
-    }
-
-    filter "system:windows"
-        cppdialect "C++17"
+    
+	filter "system:windows"
         systemversion "latest"
-
-        defines{
-            "ME_PLATFORM_WINDOWS",
-            "ME_BUILD_DLL",
-            "GLFW_INCLUDE_NONE",
-        }
-
-        postbuildcommands{
-             ("{copy} %{cfg.buildtarget.relpath} \"../_build/out-bin/" .. output_folder .. "/SandBox/\"")
-        }
-
-
-    filter "configurations:Debug"
-        defines "ME_DEBUG"
-        runtime "Debug"
-        symbols "On"
-
-    filter "configurations:Release"
-        defines "ME_RELEASE"
-        runtime "Release"
-        optimize "On"
-
-    filter "configurations:Dist"
-        defines "ME_DISTRIBUTE"
-        runtime "Release"
-        optimize "On"
-
-    --[[
-    filters { "system:windows", "configurations:Release" }
+        cppdialect "C++17"
+        staticruntime "On"
+        
+    filter { "system:windows", "configurations:Release" }
         buildoptions "/MT"
-    ]]
-
-
-project "SandBox"
-    location "SandBox"
-    kind "ConsoleApp"
-    language "C++"
-    staticruntime "off"
-
-    targetdir ("_build/out-bin/" .. output_folder .. "/%{prj.name}")
-    objdir ("_build/out-obj/" .. output_folder .. "/%{prj.name}")
-
-    files{
-        "%{prj.name}/src/**.h", 
-        "%{prj.name}/src/**.cpp"
-    }
-
-    includedirs{
-        "MiuEngine/3rdparty/spdlog/include",
-        "MiuEngine/src"
-    }
-
-    links { "MiuEngine" }
-
-    filter "system:windows"
-        cppdialect "C++17"
-        systemversion "latest"
-
-        defines{
-            "ME_PLATFORM_WINDOWS"
-        }
-
-    filter "configurations:Debug"
-        defines "ME_DEBUG"
-        runtime "Debug"
-        symbols "On"
-
-    filter "configurations:Release"
-        defines "ME_RELEASE"
-        runtime "Release"
-        optimize "On"
-
-    filter "configurations:Dist"
-        defines "ME_DISTRIBUTE"
-        runtime "Release"
-        optimize "On"
